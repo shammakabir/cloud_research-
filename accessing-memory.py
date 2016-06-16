ecca800ff42271e1e750d8908c4dd7ba38f4b027
shammakabir <shammakabir@utexas.edu>
2016-06-16 12:51:32 -0500

    Update accessing-memory.py

--- accessing-memory.py
+++ accessing-memory.py
@@ -1,11 +1,11 @@
 import os
 import random
 def main():
-    c = 1000000
-    while c > 0:
     a = random.sample(range(1,1000),100)
     print(a)
-    b = random.randint(1,99)
+    c = 1000000
+    while c > 0:
+    b = random.randint(0,99)
     print (a[b])
     c = c - 1
 main()
