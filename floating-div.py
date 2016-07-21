8083bdae293bc7ecc06f5bb02a8ff8d4264885e9
shammakabir <shammakabir@utexas.edu>
2016-07-21 16:06:18 -0500

    Update floating-div.py

--- floating-div.py
+++ floating-div.py
@@ -4,11 +4,11 @@ def main():
     d = 1000000
     while d > 0:
         a = random.uniform(0.0,100.0)
-        print(a)
+        #print(a)
         b = random.uniform(0.0,100.0)
-        print(b)
+       # print(b)
         c = a/b
-        print(c)
+        #print(c)
         d = d - 1
     with open('/proc/self/statm') as f:
         print f.readline().split()[0]
