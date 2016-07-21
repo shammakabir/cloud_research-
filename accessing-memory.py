c0568705905a41cae9a6c53140e80e9a8ce39393
shammakabir <shammakabir@utexas.edu>
2016-07-21 16:09:00 -0500

    Update accessing-memory.py

--- accessing-memory.py
+++ accessing-memory.py
@@ -2,11 +2,11 @@ import os
 import random
 def main():
     a = random.sample(range(1,1000),100)
-    #print(a)
+    print(a)
     c = 1000000
     while c > 0:
         b = random.randint(0,99)
-        #print (a[b])
+        print (a[b])
         c = c - 1
     with open('/proc/self/statm') as f:
         print f.readline().split()[0]
